d6dcd6978651fcbed6a3e85a46e12340a25de7d2
R <r@Rs-MacBook-Air.local>
2023-11-29 05:02:08 +0000

    add reactive path planning

---
+++ src/reactive_path_planning/bug_algorithm/bug.py
@@ -0,0 +1,12 @@
+import rospy
+
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import LaserScan
+from nav_msgs.msg import Odometry
+
+class Bug():
+    def __init__(self):
+        pass
+
+if __name__ == '__main__':
+    bug = Bug()
